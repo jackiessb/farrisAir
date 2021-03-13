2526778478028669625f879528cf8200161785c6
jackiessb <memjkt@gmail.com>
2021-03-13 13:43:24 -0600

    updated README

--- farrisAir.py
+++ farrisAir.py
@@ -1,11 +1,11 @@
 from flask import Flask
+# using mssql
 
 main = Flask(__name__)
-main.run(host='0.0.0.0')
 
 @main.route('/home')
 def index():
-    return "test"
+    return "zach is a genius."
 
 if __name__ == "__main__":
-    main.run(debug=False)
+    main.run(debug=False,host='0.0.0.0')    
