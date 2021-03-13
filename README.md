6030f72152bb8a6ac7718664db7915cc3cb3fbe5
Zachary Humphrey <zacharyjhumphrey@gmail.com>
2021-03-13 15:04:50 -0600

    Adding basic front end

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # farrisAir
 A custom AirDrop system for Farris Hall.
+
+## Development
+Follow these commands to start the production build
+(Make sure that npm is installed)
+
+(To build the files)
+cd client
+npm i
+npm run build
+
+(To run the server make sure you are in the root directory)
+python3 farrisAir.py
--- farrisAir.py
+++ farrisAir.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask import render_template
 
-main = Flask(__name__)
-main.run(host='0.0.0.0')
+main = Flask(__name__, template_folder="client/build", static_folder="client/build/static")
+
+@main.route('/render-page')
+def render_page(): 
+    return render_template('index.html')
 
 @main.route('/home')
 def index():
-    return "test"
+    return "HOME PAGE"
 
 if __name__ == "__main__":
-    main.run(debug=False)
+    main.run(debug=False,host='0.0.0.0')
